6f112416b4b388e2d68e99696fae834f7d8f63d2
Carla Sousa <sousa.carla09@gmail.com>
2020-10-06 17:14:22 -0300

    Initial configs

---
+++ src/app/galeria-component/galeria-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-galeria-component',
+  templateUrl: './galeria-component.component.html',
+  styleUrls: ['./galeria-component.component.css']
+})
+export class GaleriaComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
