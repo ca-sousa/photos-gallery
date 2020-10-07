0af53566f0628293ea001dc1a37a8dc48c24762a
Carla Sousa <sousa.carla09@gmail.com>
2020-10-06 21:03:35 -0300

    Create First Photo Function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  titulo = 'Cachorros';
-  fotos = [
-    '../assets/dogs/1.jpg',
-    '../assets/dogs/2.jpg',
-    '../assets/dogs/3.jpg',
-    '../assets/dogs/4.jpg',
-    '../assets/dogs/5.jpg',
-    '../assets/dogs/6.jpg',
-  ];
+  titulo = '';
+  fotos = [];
 }
--- src/app/galeria-component/galeria-component.component.ts
+++ src/app/galeria-component/galeria-component.component.ts
@@ -5,7 +5,23 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './galeria-component.component.html',
   styleUrls: ['./galeria-component.component.css']
 })
-export class GaleriaComponentComponent {
+export class GaleriaComponentComponent implements OnInit{
+  ngOnInit(): void {
+    this.index = 0;
+    this.fotoAtual = this.fotos[this.index]
+    this.status = `${this.index + 1} de ${this.fotos.length}`
+  }
+
   @Input() titulo: string;
-  @Input() fotos: [];
+  @Input() fotos: string[];
+
+  fotoAtual: string;
+  index: number;
+  status: string;
+
+  primeiraFoto() {
+    this.index = 0;
+    this.fotoAtual = this.fotos[this.index]
+    this.status = `${this.index + 1} de ${this.fotos.length}`
+  }
 }
